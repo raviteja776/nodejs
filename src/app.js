39de4d3a5e877b0af2b48ce461e7e6189242273f
Ravi Teja <raviteja776@gmail.com>
2024-11-26 08:58:31 +0530

    Routes Logic

--- src/app.js
+++ src/app.js
@@ -10,10 +10,38 @@ app.listen(3000,() => {
     console.log("Server is listening on 3000 port successfully");
 });
 
+app.get("/user", (req,res) => {
+    res.send({firstName:"Ravi" ,lastName:"Teja"});
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data Saved Successfully to the Database");
+});
+
+app.patch("/user", (req,res) => {
+    res.send("Patch API Updated");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Delete API Updated");
+});
+
 app.use("/test",(req, res) => {
     res.send("Test Path from the server");
 });
 
-app.use("/hello",(req, res) => {
-    res.send("Hello Path from the server");
+app.get("/user/:userId", (req,res)=> {
+    //params - req.params,
+    //query - req.query
+    console.log("Request",req.params, req.query);
+    res.send({firstName:"Ravi" ,lastName:"Teja"});
 });
+/** 
+
+? - Optional
++ - same key multiple
+* - any key multiple 
+
+.*fly$ - match
+((/a/)) - any letter with a matches
+*/
